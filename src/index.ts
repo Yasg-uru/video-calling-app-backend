aece81f4ad42f8a70d70b0bed5e1adeb2bf96568
yasg-uru <yashpawar12122004@gmail.com>
2024-06-03 21:02:10 +0530

    correcttion

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ app.use(cors({
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*"
+    origin: "*",
+    methods:["GET","POST"]
   },
 });
 io.on("connection", (socket) => {
