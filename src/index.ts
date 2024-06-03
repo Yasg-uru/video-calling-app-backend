1d498d6366a2844c8cac4c136c6b9644d1150a05
yasg-uru <yashpawar12122004@gmail.com>
2024-06-03 20:44:50 +0530

    correction in cors

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const io = new Server(server, {
   cors: {
     origin: "https://video-calling-app-frontend.vercel.app",
     methods: ["GET", "POST"],
+    credentials: true, 
   },
 });
 io.on("connection", (socket) => {
